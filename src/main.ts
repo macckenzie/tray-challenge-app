6cee293d023bc5fce116a8ed35fada4e2b9018bf
Nathan Macck <nathan.mendes.ti@gmail.com>
2025-06-27 11:24:24 -0300

    feat: add bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faEye, faEyeSlash, faTrash, faPen, faPlus } from '@fortawesome/free-solid-svg-icons'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const app = createApp(App)
 
