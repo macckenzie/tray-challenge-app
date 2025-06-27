58be83cbc3db0574bd8fa6ef8774566e27dd0fa6
Nathan Macck <nathan.mendes.ti@gmail.com>
2025-06-27 00:19:05 -0300

    feat: add fontawesome icons, add dayjs

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faEye, faEyeSlash, faTrash, faPen, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 const app = createApp(App)
 
+library.add(faEye, faEyeSlash, faTrash, faPen, faPlus)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router)
 
 app.mount('#app')
