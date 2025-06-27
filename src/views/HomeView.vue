4626e93b24131ac49c784df35d23a1471fa7f042
Nathan Macck <nathan.mendes.ti@gmail.com>
2025-06-27 12:27:20 -0300

    feat: add Home description

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="row">
+    <div class="col-md-12">
+      <h5>Features</h5>
+      <ul>
+        <li>Visualizar a lista completa de vendedores e suas vendas. Permite envio do e-mail de comissão do dia
+          anterior
+        </li>
+        <li>Visualizar a lista completa de vendas.</li>
+        <li>Atualizar informações de vendedores e vendas.</li>
+      </ul>
+
+      <h5>Melhorias</h5>
+      <ul>
+        <li>Login de usuário</li>
+        <li>Estilização</li>
+        <li>Paginação de resultados (vendas/vendedores) - requer alteração na API ->paginate()</li>
+      </ul>
+    </div>
+  </div>
 </template>
