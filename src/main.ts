f97844577efbd6c2a5db2a74324ebd65b390349f
Nathan Macck <nathan.mendes.ti@gmail.com>
2025-06-27 12:27:42 -0300

    feat: add faEnvelope icon

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import App from './App.vue'
 import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faEye, faEyeSlash, faTrash, faPen, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faEye, faEyeSlash, faTrash, faPen, faPlus, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const app = createApp(App)
 
-library.add(faEye, faEyeSlash, faTrash, faPen, faPlus)
+library.add(faEye, faEyeSlash, faTrash, faPen, faPlus, faEnvelope)
 
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router)
